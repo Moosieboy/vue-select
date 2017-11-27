5c90abea7d94519e273d24f4548fceca1afc174e
Moosieboy <basbuur94@gmail.com>
2017-11-27 21:46:58 +0100

    Added filter option

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -477,6 +477,26 @@
           return option;
         }
       },
+      
+      /**
+       * Tells vue-select what key to use when filtering visible options.
+       * Specify `filter-key` override default behavior of using `label` for filtering.
+       * @type {String}
+       */
+      filterKey: {
+        type: String
+      },
+      /**
+       * Callback to override default filter logic.
+       * For instance you could use Levenshtein for "fuzzy" matching of user input.
+       * @type {Function}
+       */
+      filter: {
+        type: Function,
+        default(candidate, userInput) {
+          return candidate.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+        }
+      },
 
       /**
        * An optional callback function that is called each time the selected
@@ -904,13 +924,16 @@
        * @return {array}
        */
       filteredOptions() {
+        let filterProp = this.filterKey || this.label
         let options = this.mutableOptions.filter((option) => {
-          if (typeof option === 'object' && option.hasOwnProperty(this.label)) {
-            return option[this.label].toLowerCase().indexOf(this.search.toLowerCase()) > -1
-          } else if (typeof option === 'object' && !option.hasOwnProperty(this.label)) {
-            return console.warn(`[vue-select warn]: Label key "option.${this.label}" does not exist in options object.\nhttp://sagalbot.github.io/vue-select/#ex-labels`)
+          let val = option
+          if (typeof option === 'object') {
+            if (!option.hasOwnProperty(filterProp)) {
+              return console.warn(`[vue-select warn]: filter key "option.${filterProp}" does not exist in options object.\nhttp://sagalbot.github.io/vue-select/#ex-labels`)
+            }
+            val = option[filterProp]
           }
-          return option.toLowerCase().indexOf(this.search.toLowerCase()) > -1
+          return this.filter(val, this.search, option)
         })
         if (this.taggable && this.search.length && !this.optionExists(this.search)) {
           options.unshift(this.search)
